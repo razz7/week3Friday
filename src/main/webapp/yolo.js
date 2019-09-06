ccea2deadb1de9146a22d10d0c07a25afffa597e
razz7 <rashem2@hotmail.com>
2019-09-06 13:15:34 +0200

    a

--- src/main/webapp/yolo.js
+++ src/main/webapp/yolo.js
@@ -2,9 +2,14 @@
 let url = "https://rhemmingsen.com/rest-jpa-devops-starter/api/movie/all"
 
 function click() {
-    fetch(url).then(resposne => response.json())
+    fetch(url).then(res => res.json())
             .then(data => {
-                document.getElementById("text").innerHTML ="<table><th>Film</th><td></td></table>";
+                var movies = data.map(movies => "<tr><td>" + movies.name + "</td>" +
+                "<td>" + movies.rating + "</td>" +
+                "<td>" + movies.year + "</td></tr>");
+                console.log("clicked");
+                var movList = movies.join("\n");
+                document.getElementById("tableB").innerHTML = movList;
     })
 }
 var btn = document.getElementById("btn");
