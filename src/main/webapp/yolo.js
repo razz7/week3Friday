10e00ba27e5eb446a3e6eabf2743009c8f4cf495
razz7 <rashem2@hotmail.com>
2019-09-06 12:27:20 +0200

    a

--- src/main/webapp/yolo.js
+++ src/main/webapp/yolo.js
@@ -1,7 +1,11 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+let url = "http://localhost:8080/jpareststarter/api/movie/all"
 
+function click() {
+    fetch(url).then(resposne => response.json())
+            .then(data => {
+                document.getElementById("text").innerHTML ="<table><th>Film</th><td></td></table>";
+    })
+}
+var btn = document.getElementById("btn");
+btn.onclick = click;
